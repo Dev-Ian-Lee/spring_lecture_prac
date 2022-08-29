abf9ce877af0e4c53fc49509cfca0ae16a583f88
desfox <desfox99@naver.com>
2022-08-29 23:37:13 +0900

    Added codes of HTTP request parameter, message examples

--- mvc1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ mvc1/springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -76,6 +78,23 @@ public class RequestParamController {
         Object username = paramMap.get("username");
         Object age = paramMap.get("age");
         log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("username={}", helloData);
+
+        return "ok";
+    }
+
+    // @ModelAttribute 어노테이션 생략 가능
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("username={}", helloData);
+
         return "ok";
     }
 
